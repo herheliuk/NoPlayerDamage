56a8e4ee6698ab8e44346b346ae15221a06ebe61
Andrii Herheliuk <andrii@herheliuk.com>
2026-03-28 19:52:55 +0000

    1.1.7

---
+++ NoPlayerDamage.cs
@@ -0,0 +1,41 @@
+namespace Oxide.Plugins;
+
+[Info("No Player Damage", "&anhe", "1.1.7")]
+[Description("Prevents damage from players to players unless they are in a team.")]
+public class NoPlayerDamage : RustPlugin
+{
+    private object OnEntityTakeDamage(BaseCombatEntity target, HitInfo info) =>
+        (
+            // Attacker is Player
+            info.InitiatorPlayer is {} attacker &&
+            (
+                // PvP
+                (
+                    // Target is Player
+                    target is BasePlayer victim &&
+                    // Not an NPC
+                    !victim.IsNpc &&
+                    // Not a team
+                    (
+                        attacker.currentTeam == 0 ||
+                        attacker.currentTeam != victim.currentTeam
+                    )
+                ) ||
+                // Body gathering
+                (
+                    // Target is a Corpse
+                    target is LootableCorpse corpse &&
+                    // Not an NPC
+                    corpse.playerSteamID > 76561197960265728 &&
+                    // Not self
+                    attacker.userID != corpse.playerSteamID &&
+                    // Not a team
+                    RelationshipManager.ServerInstance.FindTeam(attacker.currentTeam)
+                        ?.members.Contains(corpse.playerSteamID) != true
+                )
+            ) &&
+            // Not admin
+            !attacker.IsAdmin
+        )
+            ? false : null;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Plugin requires no configuration and works out of the box.
+
+Useful for converting a PvP server into a PvE server without setting `server.pve` to true.
